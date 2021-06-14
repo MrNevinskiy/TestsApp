efd5a8a4f1700bb7371f10626d0a692cf3187386
Aleksandr <mr.nevniskiy@gmail.com>
2021-06-14 10:14:19 +0300

    new_test_app

---
+++ app/src/main/java/com/geekbrains/tests/presenter/details/DetailsPresenter.kt
@@ -0,0 +1,33 @@
+package com.geekbrains.tests.presenter.details
+
+import com.geekbrains.tests.view.ViewContract
+import com.geekbrains.tests.view.details.ViewDetailsContract
+
+internal class DetailsPresenter internal constructor(
+    private var count: Int = 0
+) : PresenterDetailsContract {
+
+    private var viewContract: ViewDetailsContract? = null
+
+    override fun onAttach(viewContract: ViewContract) {
+        this.viewContract = viewContract as ViewDetailsContract
+    }
+
+    override fun onDetach() {
+        viewContract = null
+    }
+
+    override fun setCounter(count: Int) {
+        this.count = count
+    }
+
+    override fun onIncrement() {
+        count++
+        viewContract?.setCount(count)
+    }
+
+    override fun onDecrement() {
+        count--
+        viewContract?.setCount(count)
+    }
+}
