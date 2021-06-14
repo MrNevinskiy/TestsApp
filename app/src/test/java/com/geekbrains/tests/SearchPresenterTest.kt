8f8142e6153c950327f02991f65d44afc919c27c
Aleksandr <mr.nevniskiy@gmail.com>
2021-06-14 10:30:51 +0300

    hw

--- app/src/test/java/com/geekbrains/tests/SearchPresenterTest.kt
+++ app/src/test/java/com/geekbrains/tests/SearchPresenterTest.kt
@@ -149,4 +149,23 @@ class SearchPresenterTest {
         //Убеждаемся, что ответ от сервера обрабатывается корректно
         verify(viewContract, times(1)).displaySearchResults(searchResults, 101)
     }
+
+    @Test
+    fun viewContract_NotNull_afterOnAttach(){
+        val privateViewContract = presenter.javaClass.getDeclaredField("viewContract").apply {
+            this.isAccessible = true
+        }
+        val viewContract = privateViewContract.get(presenter)
+        assertNotNull(viewContract)
+    }
+
+    @Test
+    fun viewContract_Null_afterOnDetach() {
+        presenter.onDetach()
+        val privateViewContract = presenter.javaClass.getDeclaredField("viewContract").apply {
+            this.isAccessible = true
+        }
+        val viewContract = privateViewContract.get(presenter)
+        assertNull(viewContract)
+    }
 }
