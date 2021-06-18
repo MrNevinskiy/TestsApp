abb1590df6fb194f8f8bfc2bb7f8c246f453cd60
Aleksandr <mr.nevniskiy@gmail.com>
2021-06-18 18:21:50 +0300

    hw5

--- app/src/androidTest/java/com/geekbrains/tests/espresso/MainActivityEspressoTest.kt
+++ app/src/androidTest/java/com/geekbrains/tests/espresso/MainActivityEspressoTest.kt
@@ -1,8 +1,6 @@
-package com.geekbrains.tests
+package com.geekbrains.tests.espresso
 
 import android.view.View
-import android.widget.TextView
-import androidx.lifecycle.Lifecycle
 import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.UiController
@@ -11,7 +9,9 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import junit.framework.TestCase
+import com.geekbrains.tests.BuildConfig
+import com.geekbrains.tests.R
+import com.geekbrains.tests.view.search.MainActivity
 import org.hamcrest.Matcher
 import org.junit.After
 import org.junit.Before
@@ -29,57 +29,25 @@ class MainActivityEspressoTest {
     }
 
     @Test
-    fun activity_AssertNotNull() {
-        scenario.onActivity {
-            TestCase.assertNotNull(it)
-        }
-    }
-
-    @Test
-    fun activity_IsResumed() {
-        TestCase.assertEquals(Lifecycle.State.RESUMED, scenario.state)
-    }
-
-    @Test
-    fun activityEditText_NotNull() {
-        scenario.onActivity {
-            val searchEditText = it.findViewById<TextView>(R.id.searchEditText)
-            TestCase.assertNotNull(searchEditText)
-        }
-    }
-
-    @Test
-    fun activityEditText_IsDisplayed() {
-        onView(withId(R.id.searchEditText)).check(matches(isDisplayed()))
-    }
-
-    @Test
-    fun activityEditText_IsCompletelyDisplayed() {
-        onView(withId(R.id.searchEditText)).check(matches(isCompletelyDisplayed()))
-    }
-
-    @Test
-    fun activityButtons_AreEffectiveVisible() {
-        onView(withId(R.id.toDetailsActivityButton)).check(matches(withEffectiveVisibility(Visibility.VISIBLE)))
-    }
-
-    @Test
     fun activitySearch_IsWorking() {
         onView(withId(R.id.searchEditText)).perform(click())
         onView(withId(R.id.searchEditText)).perform(replaceText("algol"), closeSoftKeyboard())
         onView(withId(R.id.searchEditText)).perform(pressImeActionButton())
 
-        onView(isRoot()).perform(delay())
-        onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 2424")))
-
+        if (BuildConfig.TYPE == MainActivity.FAKE) {
+            onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 42")))
+        } else {
+            onView(isRoot()).perform(delay())
+            onView(withId(R.id.totalCountTextView)).check(matches(withText("Number of results: 2283")))
+        }
     }
 
     private fun delay(): ViewAction? {
         return object : ViewAction {
             override fun getConstraints(): Matcher<View> = isRoot()
-            override fun getDescription(): String = "wait for $10 seconds"
+            override fun getDescription(): String = "wait for $2 seconds"
             override fun perform(uiController: UiController, v: View?) {
-                uiController.loopMainThreadForAtLeast(10000)
+                uiController.loopMainThreadForAtLeast(2000)
             }
         }
     }
